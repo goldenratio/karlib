e1fb8899a037e9d9b7c5e764f708c62398e57a1d
goldenratio <karthikeyanvj@gmail.com>
2025-09-12 23:40:45 +0300

    update README

--- examples/main.ts
+++ examples/main.ts
@@ -1,3 +1,8 @@
+import { BrowserEnv, Karlib } from "../src";
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
 async function main(): Promise<void> {
   const example_module_name = process.env.EXAMPLE_MODULE_NAME;
   if (typeof example_module_name !== "string") {
@@ -5,7 +10,40 @@ async function main(): Promise<void> {
   }
 
   const { main: example_main } = await import(`./${example_module_name}.ts`);
-  await example_main();
+
+  const canvas = document.createElement("canvas");
+  canvas.width = GAME_WIDTH;
+  canvas.height = GAME_HEIGHT;
+
+  Object.assign(canvas.style, <CSSStyleDeclaration>{
+    // Style for centering
+    display: "block",
+    margin: "auto",
+    position: "absolute",
+    top: "0",
+    bottom: "0",
+    left: "0",
+    right: "0",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  });
+
+
+  Object.assign(document.body.style, <CSSStyleDeclaration>{
+    backgroundColor: "#6d8891",
+    margin: "0",
+    height: "100vh",
+    overflow: "hidden",
+  });
+
+  document.body.appendChild(canvas);
+
+  const kl = new Karlib({
+    canvas,
+    env: new BrowserEnv(),
+  });
+
+  await example_main(kl, canvas);
 }
 
 main();
--- examples/shape_2d.ts
+++ examples/shape_2d.ts
@@ -1,3 +1,19 @@
-export async function main(): Promise<void> {
-  console.log("foo form example!");
+import { BrowserTicker, type Karlib } from "../src";
+
+export async function main(kl: Karlib, canvas: HTMLCanvasElement): Promise<void> {
+
+  let x = 0;
+  const ticker = new BrowserTicker();
+
+  ticker.on_tick((delta) => {
+    kl.clear_background("#000");
+    kl.draw_rectangle({
+      x: x,
+      y: 100,
+      width: 100,
+      height: 100,
+    });
+
+    x += 10 * delta;
+  });
 }
