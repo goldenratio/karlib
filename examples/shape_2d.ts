b36981d4a9b36e9dad0466b15c5c4bf17c4b4cf0
goldenratio <karthikeyanvj@gmail.com>
2025-09-13 00:25:50 +0300

    update shape_2d

--- examples/shape_2d.ts
+++ examples/shape_2d.ts
@@ -1,19 +1,48 @@
 import { BrowserTicker, type Karlib } from "../src";
 
 export async function main(kl: Karlib, canvas: HTMLCanvasElement): Promise<void> {
+  const right = canvas.width;
+  const bottom = canvas.height;
+  const top = 0;
+  const left = 0;
+  const rect_width = 100;
+  const rect_height = 100;
 
-  let x = 0;
+  let x = canvas.width / 2 - 50;
+  let y = canvas.height / 2 - 50;
+  let vx = Math.random() * 10 - 5;
+  let vy = Math.random() * 10 - 5;
+
   const ticker = new BrowserTicker();
 
   ticker.on_tick((delta) => {
+    // update
+    x += vx * delta;
+    y += vy * delta;
+
+    if (x + rect_width >= right) {
+      x = right - rect_width;
+      vx *= -1;
+    } else if (x <= left) {
+      x = left;
+      vx *= -1;
+    }
+
+    if (y + rect_height >= bottom) {
+      y = bottom - rect_height;
+      vy *= -1;
+    } else if (y <= top) {
+      y = top;
+      vy *= -1;
+    }
+
+    // draw
     kl.clear_background("#000");
     kl.draw_rectangle({
-      x: x,
-      y: 100,
-      width: 100,
-      height: 100,
+      x: x | 0,
+      y: y | 0,
+      width: rect_width,
+      height: rect_height,
     });
-
-    x += 10 * delta;
   });
 }
