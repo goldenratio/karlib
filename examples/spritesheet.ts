b9996916ee04f2abfaad5f5eb151a30bd568942b
goldenratio <karthikeyanvj@gmail.com>
2025-09-24 21:31:22 +0300

    cleanup

--- examples/spritesheet.ts
+++ examples/spritesheet.ts
@@ -23,21 +23,21 @@ export async function main(canvas: HTMLCanvasElement): Promise<void> {
       texture: "hero-ship",
       x: 100,
       y: 100,
-      // scale: 2
+      scale: 2
     });
 
     kl.draw_texture({
       texture: "ship-2",
       x: 300,
       y: 300,
-      // scale: 2
+      scale: 2
     });
 
     kl.draw_texture({
       texture: "meteor-3",
       x: 500,
       y: 100,
-      // scale: 2
+      scale: 2
     });
 
   });
